d994c64ddc8b3ae414c6b25f31287d3e710b1661
gemava93 <gema_42@hotmail.com>
2025-02-13 21:13:01 +0100

    Funciones de resta y multiplicación añadidas

--- calculadoraED/src/calculadoraED/calculadora.java
+++ calculadoraED/src/calculadoraED/calculadora.java
@@ -5,6 +5,13 @@ public class calculadora {
 		return a + b;
 	}
 
+	public int restar(int a, int b) { 
+		return a - b; 
+		} 
+		public int multiplicar(int a, int b) { 
+		return a * b; 
+		} 
+		
 	public static void main(String[] args) {
 		calculadora calc = new calculadora();
 		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
